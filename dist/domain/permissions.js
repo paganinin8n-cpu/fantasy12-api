eedd2f02b59a9ca501ec34015fb08b5e2f711eae
paganinin8n-cpu <paganinin8n@gmail.com>
2026-01-17 06:34:13 -0300

    chore: remove dist do versionamento

--- dist/domain/permissions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RolePermissions = void 0;
-// Mapeamento de roles para permissões
-exports.RolePermissions = {
-    ADMIN: ['USER_READ', 'USER_WRITE', 'ADMIN_PANEL', 'AUDIT_READ'],
-    PRO: ['USER_READ'],
-    NORMAL: []
-};
