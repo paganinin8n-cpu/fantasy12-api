a2d09bb4555a046252739b64f33a5c65d59706ea
paganinin8n-cpu <paganinin8n@gmail.com>
2026-02-04 14:18:57 -0300

    fix(backend): registrar wallet routes corretamente

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import internalRoutes from './internal';
+import walletRoutes from './wallet.routes';
 
 const router = Router();
 
 router.use('/internal', internalRoutes);
+router.use(walletRoutes);
 
 export default router;
