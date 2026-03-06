626df5732e83ee35e5953eb6056b4ebc4ad36bcd
paganinin8n-cpu <paganinin8n@gmail.com>
2026-03-06 10:08:43 -0300

    fix(matches): correct prisma client usage

--- src/services/match/get-round-matches.service.ts
+++ src/services/match/get-round-matches.service.ts
@@ -2,7 +2,7 @@ import { prisma } from '../../lib/prisma'
 
 export class GetRoundMatchesService {
   static async execute(roundId: string) {
-    const matches = await prisma.matches.findMany({
+    const matches = await prisma.match.findMany({
       where: {
         roundId
       },
