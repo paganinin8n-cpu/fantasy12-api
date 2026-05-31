6f827f937c9f3e8b7a09ef583618f09e42ff81e7
Roberson Alves <robersonalves@DBA-ONZ-Mac.local>
2026-05-31 20:52:08 -0300

    Require auth on wallet route

--- src/routes/wallet.routes.ts
+++ src/routes/wallet.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import WalletController from '../controllers/wallet.controller'
+import { authMiddleware } from '../middleware/auth.middleware'
 
 const router = Router()
 
-router.get('/api/wallet', WalletController.get)
+router.get('/api/wallet', authMiddleware, WalletController.get)
 
 export default router
